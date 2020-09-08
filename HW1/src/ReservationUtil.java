70cd2eff9c40fde9dc8d6862f96ddbf6a8c1121c
rohan <rohansurana2810@gmail.com>
2020-09-08 16:44:20 -0700

    FIX: when adding in group it is assigned to last available seats instead of first occurrence fix say no more seats for grp when full even if no seats are left, it adds as null -- group when removing group -- manifest is updated but the availibility is not updated

--- HW1/src/ReservationUtil.java
+++ HW1/src/ReservationUtil.java
@@ -30,6 +30,89 @@ public class ReservationUtil {
         this.model = Model.createNewStr();
     }
 
+    public boolean readFromExistingFile(String arg) throws IOException {
+
+
+        boolean alrExists = false;
+        File file = new File(arg);
+        String[] data;
+        if (file.exists() && !file.isDirectory()) {
+            Scanner fileScanner = new Scanner(file);
+            alrExists = true;
+            while (fileScanner.hasNext()) {
+                data = fileScanner.nextLine().split(",");
+                System.out.println(Arrays.toString(data));  // remove me
+                Reservation reservation;
+                if (data.length == 4) {
+                    //todo fixme
+                } else {
+                    //business
+                    if (data[0].length() == 2) {
+                        reservation = new Reservation(data[2], data[0], data[1], "First");
+                    } else {//economy
+                        reservation = new Reservation(data[2], data[0], data[1], "Economy");
+                    }
+                    reservations.add(reservation);
+                }
+            }
+        } else {
+            System.out.println("not exists");
+        }
+//        boolean k = file.createNewFile(); // if file already exists will do nothing
+//        FileOutputStream outFile = new FileOutputStream(file, false);
+
+
+//        boolean alrExists = false;
+//        Reservation reservation;
+//        File file = new File("Reservations.txt");
+//        if (!file.exists()) {
+//            System.out.println("No such file found, Possible reasons: First Run, or No such file exists");
+//        } else {
+//            try {
+//                FileInputStream fileInputStream = new FileInputStream(file);
+//                ObjectInputStream objInpStream = new ObjectInputStream(fileInputStream);
+//                while (fileInputStream.available() > 0) {
+//                    reservation = (Reservation) objInpStream.readObject();
+//                    reservations.add(reservation);
+//                }
+//                if (fileInputStream.available() > 0) {
+//                    System.out.println("Previous Reservations Loaded!");
+//                    alrExists = true;
+//                } else {
+//                    System.out.println("No reservations found!");
+//                }
+//                fileInputStream.close();
+//                objInpStream.close();
+//            } catch (IOException | ClassNotFoundException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        return alrExists;
+    }
+
+    public void updateModelAfterReadingFromFile() {
+        for (Reservation r : reservations) {
+            String seat = r.getSeat();
+            String serviceClass = r.getService_class();
+            if (serviceClass.equals("First")) {
+                HashMap<Integer, List<String>> first = model.getValue();
+                int num = Integer.parseInt(seat.substring(0, 1));
+                List<String> list = first.get(num);
+                List<String> modifiedList = new ArrayList<>(list);
+                modifiedList.remove(seat.substring(1, 2));
+                first.replace(num, modifiedList);
+            } else {
+                //economy
+                HashMap<Integer, List<String>> econ = model.getKey();
+                int num = Integer.parseInt(seat.substring(0, 2));
+                List<String> list = econ.get(num);
+                List<String> modifiedList = new ArrayList<>(list);
+                modifiedList.remove(seat.substring(2, 3));
+                econ.replace(num, modifiedList);
+            }
+        }
+    }
+
     public void addPassenger() throws Exception {
         System.out.println("Enter Name");
         String name = sc.nextLine();
@@ -51,6 +134,45 @@ public class ReservationUtil {
         System.out.println(reservations.size());
         reservations.forEach(x -> System.out.println(x.getSeat() + " : " + x.getName()));
 
+    }
+
+    public void addGroup() throws Exception {
+        System.out.println("Enter Group Name");
+        String groupName = sc.nextLine();
+        System.out.println("Enter Passenger Names");
+        String commaSeparatedNames = sc.nextLine();
+        String[] names = commaSeparatedNames.split(",");
+        System.out.println("Select Class [First] or [Economy]");
+        String service_class = sc.nextLine();
+        List<Reservation> grpReservations = new ArrayList<>();
+        Reservation newReservation;
+        boolean canBeAdded = false;
+        int maxAvailableSeats = 0;
+        if (service_class.equals("First")) {
+            for (int i = 0; i < 2; i++) {
+                maxAvailableSeats += model.getValue().get(i + 1).size();
+            }
+        } else {
+            //economy
+            for (int i = 9; i < 29; i++) {
+                maxAvailableSeats += model.getKey().get(i + 1).size();
+            }
+        }
+        if (names.length <= maxAvailableSeats)
+            canBeAdded = true;
+        else {
+            System.out.println("Sorry! Reservation cannot be made. Not enough seats available1");
+        }
+
+        if (canBeAdded) {
+            for (String name : names) {
+                newReservation = new Reservation(name, assignSeatForGroup(service_class), "G", service_class);
+                reservations.add(newReservation);
+                grpReservations.add(newReservation);
+                reservations.forEach(x -> System.out.println(x.getSeat() + " : " + x.getName())); // remove me
+            }
+            grpMap.put(groupName, grpReservations);
+        }
     }
 
     private String assignSeat(String service_class, String preference) throws Exception {
@@ -128,74 +250,48 @@ public class ReservationUtil {
 
     }
 
+    private String assignSeatForGroup(String service_class) throws Exception {
+        String assignedSeat = null;
+        int booked = 0;
+        if (service_class.equals("First")) {
+            HashMap<Integer, List<String>> first = model.getValue();
+            outer:
+            for (int i = 0; i < 2; i++) {
+                List<String> list = first.get(i + 1);
+                for (int j = 0; j < list.size(); j++) {
+                    String seat = list.get(j);
+                    assignedSeat = getSeatAndUpdateModel(first, i, list, j, seat);
+                    break outer;
+
+                }
+            }
+        } else if (service_class.equals("Economy")) {
+            HashMap<Integer, List<String>> econ = model.getKey();
+            outer:
+            for (int i = 9; i < 29; i++) {
+                List<String> list = econ.get(i + 1);
+                for (int j = 0; j < list.size(); j++) {
+                    String seat = list.get(j);
+                    assignedSeat = getSeatAndUpdateModel(econ, i, list, j, seat);
+                    break outer;
+                }
+            }
+
+        } else
+            throw new Exception("Invalid Input! Was Expecting First or Economy, found " + service_class);
+        return assignedSeat;
+    }
+
     private String getSeatAndUpdateModel(HashMap<Integer, List<String>> first, int i, List<String> list, int j, String seat) {
         String assignedSeat = generateSeatNo(seat, i);
         List<String> modifiedList = new ArrayList<>(list);
         modifiedList.remove(list.get(j));
         first.replace(i + 1, modifiedList);
-//        first.remove(i + 1);
-//        first.put(i + 1, modifiedList);
         return assignedSeat;
     }
 
     private String generateSeatNo(String key, int i) {
         return i + 1 + key;
-    }
-
-    public void saveToFile() {
-        List<String> toWrite = new ArrayList<>();
-        for (Reservation res : reservations) {
-            String stringBuilder = res.getSeat() +
-                    ", " +
-                    res.getType() +
-                    " , " +
-                    res.getName();
-            toWrite.add(stringBuilder);
-        }
-        try {
-            Path file = Paths.get("Reservations.txt");
-            Files.write(file, toWrite, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public void printManifest() {
-        System.out.println("Select Class [First] or [Economy]");
-        String service_class = sc.nextLine();
-        System.out.println(service_class);
-        reservations.sort(new CompareReservation());
-        for (Reservation reservation : reservations) {
-            if (reservation.getService_class().equals(service_class)) {
-                String manifest = reservation.getSeat() +
-                        ": " +
-                        reservation.getName();
-
-                System.out.println(manifest);
-            }
-        }
-    }
-
-
-    public void getAvailability() {
-        System.out.println("Select Class [First] or [Economy]");
-        String service_class = sc.nextLine();
-        HashMap<Integer, List<String>> seats;
-        if (service_class.equals("First")) {
-            seats = model.getValue();
-            for (Map.Entry<Integer, List<String>> entry : seats.entrySet()) {
-                System.out.println(entry.getKey() + ":" + entry.getValue().toString());
-            }
-        } else if (service_class.equals("Economy")) {
-
-            seats = model.getKey();
-            for (Map.Entry<Integer, List<String>> entry : seats.entrySet()) {
-                System.out.println(entry.getKey() + ":" + entry.getValue().toString());
-            }
-        } else {
-            System.out.println("Invalid Input, Expected [First] or [Economy], got " + service_class);
-        }
     }
 
     public void cancelReservation() {
@@ -260,66 +356,6 @@ public class ReservationUtil {
         }
     }
 
-    public boolean readFromExistingFile(String arg) throws IOException {
-
-
-        boolean alrExists = false;
-        File file = new File(arg);
-        String[] data;
-        if (file.exists() && !file.isDirectory()) {
-            Scanner fileScanner = new Scanner(file);
-            alrExists = true;
-            while (fileScanner.hasNext()) {
-                data = fileScanner.nextLine().split(",");
-                System.out.println(Arrays.toString(data));  // remove me
-                Reservation reservation;
-                if (data.length == 4) {
-                    //todo fixme
-                } else {
-                    //business
-                    if (data[0].length() == 2) {
-                        reservation = new Reservation(data[2], data[0], data[1], "First");
-                    } else {//economy
-                        reservation = new Reservation(data[2], data[0], data[1], "Economy");
-                    }
-                    reservations.add(reservation);
-                }
-            }
-        } else {
-            System.out.println("not exists");
-        }
-//        boolean k = file.createNewFile(); // if file already exists will do nothing
-//        FileOutputStream outFile = new FileOutputStream(file, false);
-
-
-//        boolean alrExists = false;
-//        Reservation reservation;
-//        File file = new File("Reservations.txt");
-//        if (!file.exists()) {
-//            System.out.println("No such file found, Possible reasons: First Run, or No such file exists");
-//        } else {
-//            try {
-//                FileInputStream fileInputStream = new FileInputStream(file);
-//                ObjectInputStream objInpStream = new ObjectInputStream(fileInputStream);
-//                while (fileInputStream.available() > 0) {
-//                    reservation = (Reservation) objInpStream.readObject();
-//                    reservations.add(reservation);
-//                }
-//                if (fileInputStream.available() > 0) {
-//                    System.out.println("Previous Reservations Loaded!");
-//                    alrExists = true;
-//                } else {
-//                    System.out.println("No reservations found!");
-//                }
-//                fileInputStream.close();
-//                objInpStream.close();
-//            } catch (IOException | ClassNotFoundException e) {
-//                e.printStackTrace();
-//            }
-//        }
-        return alrExists;
-    }
-
     private void addAgain(List<String> toAdd) {
         for (String seat : toAdd) {
             if (seat.length() == 2) {
@@ -340,93 +376,58 @@ public class ReservationUtil {
         }
     }
 
-    public void updateModelAfterReadingFromFile() {
-        for (Reservation r : reservations) {
-            String seat = r.getSeat();
-            String serviceClass = r.getService_class();
-            if (serviceClass.equals("First")) {
-                HashMap<Integer, List<String>> first = model.getValue();
-                int num = Integer.parseInt(seat.substring(0, 1));
-                List<String> list = first.get(num);
-                List<String> modifiedList = new ArrayList<>(list);
-                modifiedList.remove(seat.substring(1, 2));
-                first.replace(num, modifiedList);
-            } else {
-                //economy
-                HashMap<Integer, List<String>> econ = model.getKey();
-                int num = Integer.parseInt(seat.substring(0, 2));
-                List<String> list = econ.get(num);
-                List<String> modifiedList = new ArrayList<>(list);
-                modifiedList.remove(seat.substring(2, 3));
-                econ.replace(num, modifiedList);
-            }
-        }
-    }
-
-    public void addGroup() throws Exception {
-        System.out.println("Enter Group Name");
-        String groupName = sc.nextLine();
-        System.out.println("Enter Passenger Names");
-        String commaSeparatedNames = sc.nextLine();
-        String[] names = commaSeparatedNames.split(",");
+    public void printManifest() {
         System.out.println("Select Class [First] or [Economy]");
         String service_class = sc.nextLine();
-        List<Reservation> grpReservations = new ArrayList<>();
-        Reservation newReservation;
-        boolean canBeAdded = false;
-        int maxAvailableSeats = 0;
-        if (service_class.equals("First")) {
-            for (int i = 0; i < 2; i++) {
-                maxAvailableSeats += model.getValue().get(i + 1).size();
-            }
-        } else {
-            //economy
-            for (int i = 9; i < 29; i++) {
-                maxAvailableSeats += model.getKey().get(i + 1).size();
-            }
-        }
-        if (names.length <= maxAvailableSeats)
-            canBeAdded = true;
-        else {
-            System.out.println("Sorry! Reservation cannot be made. Not enough seats available1");
-        }
+        System.out.println(service_class);
+        reservations.sort(new CompareReservation());
+        for (Reservation reservation : reservations) {
+            if (reservation.getService_class().equals(service_class)) {
+                String manifest = reservation.getSeat() +
+                        ": " +
+                        reservation.getName();
 
-        if (canBeAdded) {
-            for (String name : names) {
-                newReservation = new Reservation(name, assignSeatForGroup(service_class), "G", service_class);
-                reservations.add(newReservation);
-                grpReservations.add(newReservation);
-                reservations.forEach(x -> System.out.println(x.getSeat() + " : " + x.getName())); // remove me
+                System.out.println(manifest);
             }
-            grpMap.put(groupName, grpReservations);
         }
     }
 
-    private String assignSeatForGroup(String service_class) throws Exception {
-        String assignedSeat = null;
+    public void getAvailability() {
+        System.out.println("Select Class [First] or [Economy]");
+        String service_class = sc.nextLine();
+        HashMap<Integer, List<String>> seats;
         if (service_class.equals("First")) {
-            HashMap<Integer, List<String>> first = model.getValue();
-            for (int i = 0; i < 2; i++) {
-                List<String> list = first.get(i + 1);
-                for (int j = 0; j < list.size(); j++) {
-                    String seat = list.get(j);
-                    assignedSeat = getSeatAndUpdateModel(first, i, list, j, seat);
-                }
-
+            seats = model.getValue();
+            for (Map.Entry<Integer, List<String>> entry : seats.entrySet()) {
+                System.out.println(entry.getKey() + ":" + entry.getValue().toString());
             }
         } else if (service_class.equals("Economy")) {
-            HashMap<Integer, List<String>> econ = model.getKey();
-            for (int i = 9; i < 29; i++) {
-                List<String> list = econ.get(i + 1);
-                for (int j = 0; j < list.size(); j++) {
-                    String seat = list.get(j);
-                    assignedSeat = getSeatAndUpdateModel(econ, i, list, j, seat);
-                }
-            }
 
-        } else
-            throw new Exception("Invalid Input! Was Expecting First or Economy, found " + service_class);
-        return assignedSeat;
+            seats = model.getKey();
+            for (Map.Entry<Integer, List<String>> entry : seats.entrySet()) {
+                System.out.println(entry.getKey() + ":" + entry.getValue().toString());
+            }
+        } else {
+            System.out.println("Invalid Input, Expected [First] or [Economy], got " + service_class);
+        }
+    }
+
+    public void saveToFile() {
+        List<String> toWrite = new ArrayList<>();
+        for (Reservation res : reservations) {
+            String stringBuilder = res.getSeat() +
+                    ", " +
+                    res.getType() +
+                    " , " +
+                    res.getName();
+            toWrite.add(stringBuilder);
+        }
+        try {
+            Path file = Paths.get("Reservations.txt");
+            Files.write(file, toWrite, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private String getSeatAndUpdateModelForGroup(HashMap<Integer, List<String>> first, int i, List<String> list, int j, String seat) {
@@ -449,11 +450,13 @@ public class ReservationUtil {
 //  add group support for everything
 //  add documentation
 //  read from command line cl34 and input  --fixed
-//  even if no seats are left, it adds as null
-//  fix say no more seats for grp when full
+//  even if no seats are left, it adds as null -- individual
+//  even if no seats are left, it adds as null -- group -- fixed
+//  fix say no more seats for grp when full -- fixed
 //  check if null given at any stage
-//  when adding in group it is assigned to last available seats instead of first occurrence
-//  when removing group -- manifest is updated but the availibility is not updated
+//  when adding in group it is assigned to last available seats instead of first occurrence --fixed
+//  when removing group -- manifest is updated but the availibility is not updated --fixed
+//  read from file for group reservation
 
 
 //todo corner cases
