91c1038baa4f6aca0140d99d7f2ec6b538e2a1bf
rohan <rohansurana2810@gmail.com>
2020-09-19 16:01:37 -0700

    added reoccurring

--- hw2/src/MyCalender.java
+++ hw2/src/MyCalender.java
@@ -27,6 +27,81 @@ public class MyCalender {
         this.reoccurringEvents = new HashMap<>();
     }
 
+    public void printCalender() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        int daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        System.out.println(new SimpleDateFormat("MMMM yyyy").format(calendar.getTime()));
+        System.out.println(" S  M  T  W  T  F  S");
+
+        StringBuilder initialSpace = new StringBuilder();
+        for (int i = 0; i < dayOfWeek - 1; i++) {
+            initialSpace.append("   ");
+        }
+        System.out.print(initialSpace);
+
+        for (int i = 0, dayOfMonth = 1; dayOfMonth <= daysInMonth; i++) {
+            for (int j = ((i == 0) ? dayOfWeek - 1 : 0); j < 7 && (dayOfMonth <= daysInMonth); j++) {
+                System.out.printf("%2d ", dayOfMonth);
+                dayOfMonth++;
+            }
+            System.out.println();
+        }
+    }
+
+    public void loadEvents(String args) throws FileNotFoundException {
+        File file = new File(args + ".txt");
+        if (file.exists() && !file.isDirectory()) {
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNext()) {
+                String name = fileScanner.nextLine();
+                String[] details = fileScanner.nextLine().split("\\s+");
+                if (details.length == 3) {
+                    loadAndSaveEvents(name, details[0], details[1], details[2]);
+                } else {
+                    String[] actualDate = details[3].split("/");
+                    int year = Integer.parseInt(actualDate[2]) + 2000;
+                    int dayOfMonth = Integer.parseInt(actualDate[1]);
+                    int month = Integer.parseInt(actualDate[0]);
+                    LocalDate startDate = LocalDate.of(year, month, dayOfMonth);
+                    LocalDate date = startDate;
+                    String[] actualDate1 = details[4].split("/");
+                    int year1 = Integer.parseInt(actualDate1[2]) + 2000;
+                    int dayOfMonth1 = Integer.parseInt(actualDate1[1]);
+                    int month1 = Integer.parseInt(actualDate1[0]);
+                    LocalDate endDate = LocalDate.of(year1, month1, dayOfMonth1);
+                    long weeks = ChronoUnit.WEEKS.between(date, endDate);
+                    String days = details[0];  // event days For eg . TF
+                    for (int i = 0; i < days.length(); i++) {
+                        int w = (int) weeks;
+                        date = LocalDate.of(year, month, dayOfMonth);
+                        DayOfWeek dayOfWeek = date.getDayOfWeek();  // gets the day on the given starting day
+                        String dayOfWeek2 = getDayOfWeek(days.substring(i, i + 1));  // gets the first two letters of that day
+                        boolean sub = false;
+                        while (!dayOfWeek2.equals(dayOfWeek.toString().substring(0, 2))) {
+                            date = date.plusDays(1);
+                            dayOfWeek = dayOfWeek.plus(1);
+                            sub = true;
+                        }
+                        if (!sub) {
+                            w = (int) (weeks + 1);
+                        }
+                        for (int j = 0; j < w; j++) {
+                            loadAndSaveEventsReoccurring(name, details[0], date, details[1], details[2], startDate, endDate);
+                            date = date.plusDays(7);
+                        }
+                    }
+                }
+            }
+        } else {
+            System.out.println("This is the first run! Any previous records not found.");
+        }
+        System.out.println("Successfully Loaded");
+
+    }
+
     public void createEvent() {
         System.out.println("Enter the name of the event:");
         String name = sc.nextLine();
@@ -91,14 +166,6 @@ public class MyCalender {
         }
     }
 
-    private Pair<LocalTime, LocalTime> timeFormatter(String startingTime, String endingTime) {
-        String[] timeStart = startingTime.split(":");
-        String[] timeEnd = endingTime.split(":");
-        LocalTime start = LocalTime.of(Integer.parseInt(timeStart[0]), Integer.parseInt(timeStart[1]));
-        LocalTime last = LocalTime.of(Integer.parseInt(timeEnd[0]), Integer.parseInt(timeEnd[1]));
-        return new Pair<>(start, last);
-    }
-
     private void loadAndSaveEventsReoccurring(String name, String reoccursOn, LocalDate date, String startingTime, String endingTime, LocalDate startDate, LocalDate endDate) {
         Pair<LocalTime, LocalTime> times = timeFormatter(startingTime, endingTime);
         Event event = new Event(name, reoccursOn, date, times.getKey(), times.getValue(), startDate, endDate);
@@ -136,27 +203,6 @@ public class MyCalender {
             System.out.println(entry.getKey() + "  ");
             entry.getValue().forEach(x -> System.out.println(x.getName()));
         }
-    }
-
-
-    public void quit() {
-        System.out.println("Good Bye!");
-        List<String> toWrite = new ArrayList<>();
-        for (Map.Entry<LocalDate, ArrayList<Event>> entry : events.entrySet()) {
-            for (Event e : entry.getValue()) {
-                toWrite.add(e.getName());
-                toWrite.add(
-                        e.getDate().format(DateTimeFormatter.ofPattern("MM/dd/yyyy")) + " " + e.getStartTime() + " " + e.getEndTime()
-                );
-            }
-        }
-        try {
-            Path file = Paths.get("output.txt");
-            Files.write(file, toWrite, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
     public void goTo() {
@@ -200,7 +246,6 @@ public class MyCalender {
                 );
             }
         }
-
 
         System.out.println("RECURRING EVENTS");
         List<String> names = new ArrayList<>(reoccurringEvents.keySet());
@@ -279,63 +324,44 @@ public class MyCalender {
                 break;
             }
             case "DR": {
-                // todo
+                System.out.println("Enter the name of the reoccurring event to delete");
+                String toDelete = sc.nextLine();
+                if (reoccurringEvents.containsKey(toDelete)) {
+                    reoccurringEvents.remove(toDelete);
+                    System.out.println("Successfully removed");
+                } else {
+                    System.out.println("No reoccurring events found under the given name. Please try again");
+                }
             }
         }
+    }
 
+    public void quit() {
+        System.out.println("Good Bye!");
+        List<String> toWrite = new ArrayList<>();
+        for (Map.Entry<LocalDate, ArrayList<Event>> entry : events.entrySet()) {
+            for (Event e : entry.getValue()) {
+                toWrite.add(e.getName());
+                toWrite.add(
+                        e.getDate().format(DateTimeFormatter.ofPattern("MM/dd/yyyy")) + " " + e.getStartTime() + " " + e.getEndTime()
+                );
+            }
+        }
+        try {
+            Path file = Paths.get("output.txt");
+            Files.write(file, toWrite, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public void loadEvents(String args) throws FileNotFoundException {
-        File file = new File(args + ".txt");
-        if (file.exists() && !file.isDirectory()) {
-            Scanner fileScanner = new Scanner(file);
-            while (fileScanner.hasNext()) {
-                String name = fileScanner.nextLine();
-                String[] details = fileScanner.nextLine().split("\\s+");
-                if (details.length == 3) {
-                    loadAndSaveEvents(name, details[0], details[1], details[2]);
-                } else {
-                    String[] actualDate = details[3].split("/");
-                    int year = Integer.parseInt(actualDate[2]) + 2000;
-                    int dayOfMonth = Integer.parseInt(actualDate[1]);
-                    int month = Integer.parseInt(actualDate[0]);
-                    LocalDate startDate = LocalDate.of(year, month, dayOfMonth);
-                    LocalDate date = startDate;
-                    String[] actualDate1 = details[4].split("/");
-                    int year1 = Integer.parseInt(actualDate1[2]) + 2000;
-                    int dayOfMonth1 = Integer.parseInt(actualDate1[1]);
-                    int month1 = Integer.parseInt(actualDate1[0]);
-                    LocalDate endDate = LocalDate.of(year1, month1, dayOfMonth1);
-                    long weeks = ChronoUnit.WEEKS.between(date, endDate);
-                    System.out.println(weeks);
-                    String days = details[0];  // event days For eg . TF
-                    for (int i = 0; i < days.length(); i++) {
-                        int w = (int) weeks;
-                        date = LocalDate.of(year, month, dayOfMonth);
-                        DayOfWeek dayOfWeek = date.getDayOfWeek();  // gets the day on the given starting day
-                        String dayOfWeek2 = getDayOfWeek(days.substring(i, i + 1));  // gets the first two letters of that day
-                        boolean sub = false;
-                        while (!dayOfWeek2.equals(dayOfWeek.toString().substring(0, 2))) {
-                            date = date.plusDays(1);
-                            dayOfWeek = dayOfWeek.plus(1);
-                            sub = true;
-                        }
-                        if (!sub) {
-                            w = (int) (weeks + 1);
-                        }
-                        for (int j = 0; j < w; j++) {
-                            loadAndSaveEventsReoccurring(name, details[0], date, details[1], details[2], startDate, endDate);
-                            date = date.plusDays(7);
-                        }
-                    }
-                }
-            }
-        } else {
-            System.out.println("This is the first run! Any previous records not found.");
-        }
-        System.out.println("Successfully Loaded");
-
+    private Pair<LocalTime, LocalTime> timeFormatter(String startingTime, String endingTime) {
+        String[] timeStart = startingTime.split(":");
+        String[] timeEnd = endingTime.split(":");
+        LocalTime start = LocalTime.of(Integer.parseInt(timeStart[0]), Integer.parseInt(timeStart[1]));
+        LocalTime last = LocalTime.of(Integer.parseInt(timeEnd[0]), Integer.parseInt(timeEnd[1]));
+        return new Pair<>(start, last);
     }
 
     private String getDayOfWeek(String detail) {
@@ -376,35 +402,10 @@ public class MyCalender {
 
     }
 
-    public void printCalender() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
-        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-        int daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-
-        System.out.println(new SimpleDateFormat("MMMM yyyy").format(calendar.getTime()));
-        System.out.println(" S  M  T  W  T  F  S");
-
-        StringBuilder initialSpace = new StringBuilder();
-        for (int i = 0; i < dayOfWeek - 1; i++) {
-            initialSpace.append("   ");
-        }
-        System.out.print(initialSpace);
-
-        for (int i = 0, dayOfMonth = 1; dayOfMonth <= daysInMonth; i++) {
-            for (int j = ((i == 0) ? dayOfWeek - 1 : 0); j < 7 && (dayOfMonth <= daysInMonth); j++) {
-                System.out.printf("%2d ", dayOfMonth);
-                dayOfMonth++;
-            }
-            System.out.println();
-        }
-
-    }
-
-
     //todo
     // change print calender
     // implement view by
     // introduce the concept of Recurring events everywhere
+    // fix time conflict reoccur/one-time
 
 }
